240e22d7e632eae9f6036d9565155da9d1e02018
Yotam Tsorfy Shaked <tsorfi@gmail.com>
2025-05-29 12:39:00 +0300

    minor fix in parentChildCunkFetcher.ts update the default query in fetchEligibleParentRecords

--- src/services/parentChildChunkFetcher.ts
+++ src/services/parentChildChunkFetcher.ts
@@ -139,8 +139,7 @@ async function fetchEligibleParentRecords(
      WHERE RowId > @startRow
      AND is_eligible = 1
      AND is_new = 1
-     AND Error like '%שורה 1- הכנסה לקובץ נכשלה%'
-     -- AND (Status IS NULL OR Status = 'Failed')       
+     AND (Status IS NULL OR Status = 'Failed')    
      ORDER BY RowId ASC
      OFFSET 0 ROWS
      FETCH NEXT @limit ROWS ONLY`,
