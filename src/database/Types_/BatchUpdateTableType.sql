2dd74d5f6bef1e857f3e6418a8ebbc9d676c7967
Yotam Tsorfy Shaked <tsorfi@gmail.com>
2025-06-03 13:06:04 +0300

    Refactor BatchUpdateTableType.sql to include type drop check before creation 🛠️📄§

--- src/database/Types_/BatchUpdateTableType.sql
+++ src/database/Types_/BatchUpdateTableType.sql
@@ -1,17 +1,20 @@
 USE [CarmeltonDB_STG]
 GO
 
-/****** Object:  UserDefinedTableType [dbo].[BatchUpdateTableType]    Script Date: 29/05/2025 09:18:32 ******/
-CREATE TYPE [dbo].[BatchUpdateTableType] AS TABLE(
-	[RowId] [int] NULL,
-	[BatchId] [uniqueidentifier] NULL,
-	[JobName] [nvarchar](255) NULL,
-	[Status] [nvarchar](100) NULL,
-	[Error] [nvarchar](max) NULL,
-	[JobId] [uniqueidentifier] NULL,
-	[priority_id] [nvarchar](50) NULL,
-	[is_new] [bit] NULL
-)
+-- Drop the existing type first
+IF EXISTS (SELECT * FROM sys.types WHERE name = 'BatchUpdateTableType')
+    DROP TYPE [dbo].[BatchUpdateTableType]
 GO
 
-
+CREATE TYPE [dbo].[BatchUpdateTableType] AS TABLE(
+    [RowId] [int] NULL,
+    [BatchId] [uniqueidentifier] NULL,
+    [JobName] [nvarchar](255) NULL,
+    [Status] [nvarchar](100) NULL,
+    [Error] [nvarchar](max) NULL,
+    [JobId] [uniqueidentifier] NULL,
+    [priority_id] [nvarchar](50) NULL,
+    [is_new] [bit] NULL,
+    [StatusCode] [int] NULL
+)
+GO
