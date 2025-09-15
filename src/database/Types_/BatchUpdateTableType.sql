c1af422cb5a0d01282d227f65ec33e71beeacf48
Yotam Tsorfy Shaked <tsorfi@gmail.com>
2025-09-15 08:00:06 +0300

    Add HebrewName to JobType and enhance job type selection with RTL support 🇮🇱✨; Update BatchUpdateTableType priority_id length to 255 🔄📊; Improve QueueProcessor code clarity with formatting adjustments ✨

--- src/database/Types_/BatchUpdateTableType.sql
+++ src/database/Types_/BatchUpdateTableType.sql
@@ -1,19 +1,17 @@
 USE [CarmeltonDB_PRD]
 GO
 
-/****** Object:  UserDefinedTableType [dbo].[BatchUpdateTableType]    Script Date: 07/06/2025 07:53:15 ******/
+/****** Object:  UserDefinedTableType [dbo].[BatchUpdateTableType]    Script Date: 15/09/2025 ******/
 CREATE TYPE [dbo].[BatchUpdateTableType] AS TABLE(
-	[RowId] [int] NULL,
-	[BatchId] [uniqueidentifier] NULL,
-	[JobName] [nvarchar](255) NULL,
-	[Status] [nvarchar](100) NULL,
-	[Error] [nvarchar](max) NULL,
-	[CleanError] [nvarchar](max) NULL,
-	[JobId] [uniqueidentifier] NULL,
-	[priority_id] [nvarchar](50) NULL,
-	[is_new] [bit] NULL,
-	[StatusCode] [int] NULL
+    [RowId] [int] NULL,
+    [BatchId] [uniqueidentifier] NULL,
+    [JobName] [nvarchar](255) NULL,
+    [Status] [nvarchar](100) NULL,
+    [Error] [nvarchar](max) NULL,
+    [CleanError] [nvarchar](max) NULL,
+    [JobId] [uniqueidentifier] NULL,
+    [priority_id] [nvarchar](255) NULL, -- Increased from 50 to 255
+    [is_new] [bit] NULL,
+    [StatusCode] [int] NULL
 )
 GO
-
-
